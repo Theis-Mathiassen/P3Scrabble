af873485011cfc8d23caffa72b09bb2c188cf844
Daniel Hilo Hansen <55060805+KYNIKER@users.noreply.github.com>
2022-10-23 15:09:57 +0200

    Har jeg fucked det op nu? + add bag funktion

--- Scrabble/Scrabble/Client/Bag.cs
+++ Scrabble/Scrabble/Client/Bag.cs
@@ -8,7 +8,15 @@
         }
 
         public List<CharTile> TilesInHand { get; }
+        public List<CharTile> TilesInBag { get; }
 
+        private MakeCharTiles(char character, int point, int numberOfTiles)
+        {
+            for (int i = 0; i < numberOfTiles; i++)
+			{
+                TilesInBag.Add(new CharTile(character, point));
+			}
+        }
 
         internal CharTile pickRandomTile()
         {
